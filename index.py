69a0208e99823e8cff66f70195893af32d968428
keizuka <benjamin.ardilouze212@gmail.com>
2023-01-25 15:51:18 +0100

    update 1.1

--- index.py
+++ index.py
@@ -1,5 +1,9 @@
-<!DOCTYPE html>
-<html lang="fr">
+#coding:utf-8
+import cgi
+
+print("Content type: text/html; charset=utf-8\n")
+
+html = """<!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
     <title>test serveur http python</title>
@@ -9,3 +13,5 @@
     <p>test d'un server http en python</p>
 </body>
 </html>
+"""
+print(html)
--- serverhttp.py
+++ serverhttp.py
@@ -1,15 +1,10 @@
 #coding:utf-8
-
 import http.server
-#import socketserver
-
-
 port = 80
 address = ("", port)
 server = http.server.HTTPServer
 handler = http.server.CGIHTTPRequestHandler
-handler.cgi_directories = ["/"]
+handler.cgi_directories = ["index.py"]
 httpd = server(address, handler)
-
-print("serverhttp.py start on port{port}")
+print("serverhttp.py start on port ", {port})
 httpd.serve_forever()
